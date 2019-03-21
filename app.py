a0f77af0c262d5418454a62dcc34c0ac04df52da
Jorge Heleno <jorge.heleno@tecnico.ulisboa.pt>
2019-03-21 15:26:23 +0000

    Add sharK !

--- app.py
+++ app.py
@@ -7,10 +7,15 @@ class ExampleHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("Hello world")
 
+class SharkHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("<html><img src=\"https://cdn0.vox-cdn.com/thumbor/D7eWJa6P3c7wqfBnhLMMGo4qauY=/0x0:750x422/1600x900/cdn0.vox-cdn.com/uploads/chorus_image/image/46801036/sharknado2.0.0.jpg\" /> </html>")
+
 def make_app():
     return tornado.web.Application([
         (r'/',ExampleHandler),
         (r'/example',ExampleHandler)
+        (r'/shark',SharkHandler)
         ])
 
 
